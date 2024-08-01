2d1dcec1c308d9b612e77e1da652854e781a5b18
seankennelly <148450933+seankennelly@users.noreply.github.com>
2024-08-01 17:29:01 +0100

    Delete gazetteer_v2 directory

--- gazetteer_v2/libraries/php/getAirports.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  $country_code = $_GET['countryCode'];
-
-  $data = file_get_contents("http://api.geonames.org/searchJSON?formatted=true&country=$country_code&q=airport&username=seankennelly");
-
-  print_r($data);
-
-?>
